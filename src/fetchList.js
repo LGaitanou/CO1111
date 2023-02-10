26ac289ccdbe83e4fdc9b5136e26c1fa956f6d4b
LGaitanou <LGaitanou@uclan.ac.uk>
2023-02-10 11:56:34 +0200

    Added code to the challenge page

--- src/fetchList.js
+++ src/fetchList.js
@@ -1,20 +1,23 @@
-fetch("https://codecyprus.org/th/api/list")
-    .then(response => response.json()) //Parse JSON text to JavaScript object
-    .then(jsonObject => {
-        //console.log(jsonObject.treasureHunts); //TODO - Success, do something with the data.
-        let THArray = jsonObject.treasureHunts;
-        console.log(THArray);
-        for (let i = 0; i < THArray.length; i++) {
-            let listItem = document.createElement("li");
-            let uuid = THArray[i].uuid;
-            console.log(uuid);
-            // let link = "https://codecyprus.org/th/api/start?player=Loizos&app=LOLOLOL&treasure-hunt-id=" + uuid;
-            listItem.innerHTML = "<a href=https://codecyprus.org/th/api/start?player=Loizos&app=LOLOLOL&treasure-hunt-id=>" + THArray[i].name + "</a>";
-            challengesList.appendChild(listItem);
-        }
-    });
 
-
+function getChallenges() {
+    fetch("https://codecyprus.org/th/api/list")
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+            //console.log(jsonObject.treasureHunts); //TODO - Success, do something with the data.
+            let THArray = jsonObject.treasureHunts;
+            console.log(THArray);
+            for (let i = 0; i < THArray.length; i++) {
+                let listItem = document.createElement("li");
+                let uuid = THArray[i].uuid;
+                console.log(uuid);
+                // let link = "https://codecyprus.org/th/api/start?player=Loizos&app=LOLOLOL&treasure-hunt-id=" + uuid;
+                listItem.innerHTML = "<a href=https://codecyprus.org/th/api/start?player=Loizo&app=LOLOLOL&treasure-hunt-id="+uuid+">" + THArray[i].name + "</a>";
+                challengesList.appendChild(listItem);
+            }
+        });
+}
+getChallenges();
+let challengesList = document.getElementById("challengeList");
 let body = document.body;
 
 /*

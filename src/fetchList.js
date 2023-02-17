8925d0293f5c93599f7897a1b5db2bf66698e90f
LGaitanou <LGaitanou@uclan.ac.uk>
2023-02-17 12:00:45 +0200

    Added code to the challenge page

--- src/fetchList.js
+++ src/fetchList.js
@@ -1,4 +1,6 @@
+function RemoveElement() {
 
+}
 function getChallenges() {
     fetch("https://codecyprus.org/th/api/list")
         .then(response => response.json()) //Parse JSON text to JavaScript object
@@ -10,15 +12,23 @@ function getChallenges() {
                 let listItem = document.createElement("li");
                 let uuid = THArray[i].uuid;
                 console.log(uuid);
-                // let link = "https://codecyprus.org/th/api/start?player=Loizos&app=LOLOLOL&treasure-hunt-id=" + uuid;
-                listItem.innerHTML = "<a href=https://codecyprus.org/th/api/start?player=Loizo&app=LOLOLOL&treasure-hunt-id="+uuid+">" + THArray[i].name + "</a>";
+                listItem.innerHTML = "<a href=https://codecyprus.org/th/api/start?player="+teamName+"&app=LOLOLOL&treasure-hunt-id="+uuid+">" + THArray[i].name + "</a>";
                 challengesList.appendChild(listItem);
             }
         });
 }
 getChallenges();
 let challengesList = document.getElementById("challengeList");
-let body = document.body;
+
+// This black of code handles query strings and was taken from the website stackoverflow
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+// Get the name of the team
+let teamName = params.name; // "some_value"
+
 
 /*
 let loadingDiv = document.getElementById("myDiv");

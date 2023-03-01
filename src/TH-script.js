5fbc972ad142d476382d0b8c8bd473c7ef70c786
LGaitanou <LGaitanou@uclan.ac.uk>
2023-03-01 23:32:43 +0200

    Started the javascript side of the TH by renaming some files and creating a new page for the user to enter their name

--- src/TH-script.js
+++ src/TH-script.js
@@ -1,6 +1,12 @@
+const TH_BASE_URL = "https://codecyprus.org/th/api/"; // the true API base url
+const TH_TEST_URL = "https://codecyprus.org/th/test-api/"; // the test API base url
+
+function redirect(url) {
+    window.location.href = url.toString();
+}
 
 function getChallenges() {
-    fetch("https://codecyprus.org/th/api/list")
+    fetch(TH_BASE_URL + "list")
         .then(response => response.json()) //Parse JSON text to JavaScript object
         .then(jsonObject => {
             loader.hidden = true;
@@ -11,7 +17,7 @@ function getChallenges() {
                     let listItem = document.createElement("li");
                     let uuid = THArray[i].uuid;
                     console.log(uuid);
-                    listItem.innerHTML = "<a href=https://codecyprus.org/th/api/start?player=" + teamName + "&app=Team6TH&treasure-hunt-id=" + uuid + ">" + THArray[i].name + "</a>";
+                    listItem.innerHTML = "<a href=\"javascript:select(\'" + uuid + "\')\">" + THArray[i].name + "</a>";
                     challengesList.appendChild(listItem);
                 }
             }
@@ -38,7 +44,61 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 // Get the name of the team
 let teamName = params.name;
 
+function select(uuid) {
 
+    console.log("ENTERED")
+    fetch(TH_BASE_URL + "start?player=" + teamName + "&app=TH-Team6&treasure-hunt-id=" + uuid)
+        .then(response => response.json())
+        .then(jsonObject => {
+            if (jsonObject.status === "OK") {
+                setCookie("session", jsonObject.session, 365);
+                redirect("session.html");
+            }
+            else {
 
+            }
+
+        });
+}
+
+// Functions to handle cookies taken from w3schools
+// https://www.w3schools.com/js/js_cookies.asp
+//////////////////////////////////////////////////
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
+//////////////////////////////////////
 
 

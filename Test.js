eb57922885ce6d1eb6ad36ad56ac4ae645981181
cbnduka <cbnduka@uclan.ac.uk>
2023-03-08 17:50:19 +0200

    Test-running

--- Test.js
+++ Test.js
@@ -1,14 +1,30 @@
- // Define a function that tests if a number is even or odd
-    function testNumber(number) {
-    if (number % 2 === 0) {
-    return number + " is even";
-}
-    else {
-    return number + " is odd";
-    }
-}
+//Testing the JS script
 
-    // Call the function with some test numbers
-    console.log(testNumber(4)); // Output: "4 is even"
-    console.log(testNumber(7)); // Output: "7 is odd"
-    console.log(testNumber(0)); // Output: "0 is even"
+const form = document.querySelector('form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const loginBtn = document.getElementById('login-btn');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(response => response.json())
+        .then(data => {
+            // check if login was successful or not
+            if (data.success) {
+                // redirect to the game app
+                window.location.href = '/game';
+            } else {
+                alert('Invalid email/password. Please try again.');
+            }
+        })
+        .catch(error => console.error(error));
+});

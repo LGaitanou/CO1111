e7649da33dc54b72a36be1cbd3e440d1717ccf43
Duke <cbnduka@uclan.ac.uk>
2023-03-23 15:30:25 +0200

    CheckQuestions Testing

--- Test.js
+++ Test.js
@@ -7,3 +7,35 @@ const apiEndpoints = {
     "getScore": "/th/test-api/score",
     "getLeaderboard": "/th/test-api/leaderboard"
 };
+
+function testStartQuiz() {
+    // Fetch the start quiz endpoint
+    fetch(apiEndpoints.startQuiz)
+        .then(response => {
+            if (response.ok) {
+                document.getElementById("result-startQuiz").innerHTML = "Success! Start quiz function is working.";
+            }
+            else {
+                document.getElementById("result-startQuiz").innerHTML = "Error: Start quiz function is not working.";
+            }
+        })
+        .catch(error => {
+            document.getElementById("result-startQuiz").innerHTML = "Error: " + error.message;
+        });
+}
+
+function testGetQuestion() {
+    // Fetch the get question endpoint with a sample question ID
+    fetch(apiEndpoints.getQuestion + "?question_id=123")
+        .then(response => {
+            if (response.ok) {
+                document.getElementById("result-getQuestion").innerHTML = "Success! Get question function is working.";
+            }
+            else {
+                document.getElementById("result-getQuestion").innerHTML = "Error: Get question function is not working.";
+            }
+        })
+        .catch(error => {
+            document.getElementById("result-getQuestion").innerHTML = "Error: " + error.message;
+        });
+}
